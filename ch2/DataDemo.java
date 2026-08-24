890ba8060ec51c6f9bc87720e50596ff0abf4e28
loganv123 <lvanhuffel1@ivytech.edu>
2026-08-24 16:05:33 -0400

    First commit

---
+++ ch2/DataDemo.java
@@ -0,0 +1,8 @@
+public class DataDemo
+{
+    public static void main(String[] args)
+    {
+        int aWholeNumber = 315;
+        System.out.println("The whole number is: " + aWholeNumber);
+    }
+}
