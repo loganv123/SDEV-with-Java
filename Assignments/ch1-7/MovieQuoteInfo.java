d185c12f7bece57e010211c816605c4a68af0780
loganv123 <lvanhuffel1@ivytech.edu>
2026-08-31 15:44:48 -0400

    ch1 ex7

---
+++ Assignments/ch1-7/MovieQuoteInfo.java
@@ -0,0 +1,13 @@
+// Logan Vanhuffel
+// p.35
+
+public class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Movie: Star Wars");
+        System.out.println("Quote: \"May the force be with you.\"");
+        System.out.println("Character: Obi-Wan Kenobi");
+        System.out.println("Year released: 1977");
+    }
+}
